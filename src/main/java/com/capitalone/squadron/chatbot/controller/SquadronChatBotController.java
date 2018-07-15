75100fc81628c2369d0e80a04dc385a98cb9d4bb
amit9p <amit_prasad@live.com>
2018-07-15 15:27:53 +0530

    Update SquadronChatBotController.java

--- src/main/java/com/capitalone/squadron/chatbot/controller/SquadronChatBotController.java
+++ src/main/java/com/capitalone/squadron/chatbot/controller/SquadronChatBotController.java
@@ -1,12 +1,22 @@
 package com.capitalone.squadron.chatbot.controller;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.capitalone.squadron.chatbot.service.SquadronChatBotService;
+import com.capitalone.squadron.chatbot.model.SquadronChatBotModel;
 
+import java.io.StringWriter;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.json.JsonWriter;
+
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class SquadronChatBotController {
 
@@ -18,15 +28,28 @@ public class SquadronChatBotController {
         return "Welcome to Squadron Chat Bot";
     }
 
-    @RequestMapping(value="/hello/{name}", method={RequestMethod.POST,RequestMethod.GET})
+    @RequestMapping(value = "/hello/{name}", method = {RequestMethod.POST, RequestMethod.GET}, produces = MediaType.APPLICATION_JSON_VALUE)
     public String myData(@PathVariable("name") String name) {
         return squadronChatBotService.getMessage(name);
     }
 
 
-    @RequestMapping(value="/chatbotResponse/{question}", method={RequestMethod.GET})
-    public String getResponseFromBot(@PathVariable("question") String question) throws Exception{
-        return squadronChatBotService.getResponseFromBot(question);
+    @RequestMapping(value = "/chatbotResponse/{question}", method = {RequestMethod.GET})
+    public String getResponseFromBot(@PathVariable("question") String question) throws Exception {
+
+        JsonObjectBuilder jsonBuilder = Json.createObjectBuilder();
+        jsonBuilder.add("bot_reply", squadronChatBotService.getResponseFromBot(question));
+        JsonArrayBuilder plnArrBld = Json.createArrayBuilder();
+        JsonArray arr = plnArrBld.build();
+        JsonObject empObj = jsonBuilder.build();
+        StringWriter strWtr = new StringWriter();
+        JsonWriter jsonWtr = Json.createWriter(strWtr);
+        jsonWtr.writeObject(empObj);
+        jsonWtr.close();
+        return "["+strWtr.toString()+"]";
+
+
+
     }
 
 
